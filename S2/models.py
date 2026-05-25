94abedc33267a8514be3b5a9cf35a66cb7074061
Daniil-Mah <daniilmahin17@gmail.com>
2026-05-25 08:43:27 +0300

    Update models.py

--- S2/models.py
+++ S2/models.py
@@ -1,3 +1,4 @@
+import re
 from peewee import *
 
 db = SqliteDatabase('S2_Profil_datebase.db')
@@ -9,11 +10,18 @@ class BaseModel(Model):
 class Profil(BaseModel):
     first_name = CharField()
     second_name = CharField()
-    last_name = CharField()
-    telephone = IntegerField()
-    e_mail = CharField()
-    photo = CharField()
-    profil_act = BooleanField(default=True)
+    last_name = CharField(default=Null)
+    telephone = CharField(max_length=16)
+    email = CharField()
+    path_to_photo = CharField()
+    is_active = BooleanField(default=True)
+    
+    def save(self, *args, **kwargs):
+        pattern = r'^\+7\d{14}$'
+        if not re.match(pattern, self.phone):
+            raise ValueError("Неверный формат номера телефона. Ожидается: +7(XXX)XXX-XX-XX")
+        
+        return super().save(*args, **kwargs)
 
 class NotificationSettings(BaseModel):
     notification_turn = BooleanField(default=True)
