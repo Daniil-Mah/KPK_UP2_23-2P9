768fa67429c90043d14a1f484bab15e65fe09ab8
Daniil-Mah <daniilmahin17@gmail.com>
2026-06-01 22:03:17 +1400

    Update models.py

--- S2/models.py
+++ S2/models.py
@@ -9,17 +9,10 @@ class BaseModel(Model):
 
 class Profile(BaseModel):
     full_name = CharField()
-    telephone = CharField(max_length=16, unique=True)
+    telephone = CharField(max_length=10, unique=True)
     email = CharField(max_length=254, unique=True)
     path_to_photo = CharField()
     is_active = BooleanField(default=True)
-    
-    def save(self, *args, **kwargs):
-        pattern = r'^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$'
-        if not re.match(pattern, self.telephone):
-            raise ValueError("Неверный формат номера телефона. Ожидается: +7(XXX)XXX-XX-XX")
-        
-        return super().save(*args, **kwargs)
 
 class NotificationSettings(BaseModel):
     profile_id = ForeignKeyField(Profile, backref='notification_settings')
