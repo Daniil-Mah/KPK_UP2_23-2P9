d0ff31213eb1e87c2125c281008841d5ee0f03e8
Daniil-Mah <daniilmahin17@gmail.com>
2026-06-01 21:49:33 +1400

    Update models.py

--- S2/models.py
+++ S2/models.py
@@ -15,7 +15,7 @@ class Profile(BaseModel):
     is_active = BooleanField(default=True)
     
     def save(self, *args, **kwargs):
-        pattern = r'^\+7$\($\d{3}$\)$\d{3}-\d{2}-\d{2}$'
+        pattern = r'^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$'
         if not re.match(pattern, self.telephone):
             raise ValueError("Неверный формат номера телефона. Ожидается: +7(XXX)XXX-XX-XX")
         
